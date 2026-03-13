bff634383ebca5e86c6f09192df5fa29ac2cb538
Md. Kamran Alam <ka6923442@gmail.com>
2026-03-14 04:07:06 +0530

    Initial Commit

--- APIGateway/routes/auth.route.js
+++ APIGateway/routes/auth.route.js
@@ -5,13 +5,12 @@ const router = Router();
 
 const AUTH_SERVICE_URL = "http://localhost:5001";
 
-
 router.use((req, res, next) => {
     console.log("Gateway forwarding to ", req.method, req.url);
     next();
 });
 
-router.use(createProxyMiddleware({
+router.use("/", createProxyMiddleware({
     target : AUTH_SERVICE_URL,
     changeOrigin : true,
     pathRewrite : {
